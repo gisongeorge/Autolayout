1ec391e8df25f0d93a2b12c32d622d358b4347d0
Gison <gisongeorge@gmail.com>
2017-05-29 15:16:06 +0530

    Add files via upload

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample_AutoLayout
+//
+//  Created by Gison on 24/05/17.
+//  Copyright © 2017 JSV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Sample_AutoLayout
+//
+//  Created by Gison on 24/05/17.
+//  Copyright © 2017 JSV. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
